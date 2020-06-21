067ab662d1ac34612f9def87cffc18774efc5bc5
surimLee <45868707+surimLee@users.noreply.github.com>
2020-06-21 21:42:18 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/kpu/ce2016152030/spgptermproj/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.kpu.ce2016152030.spgptermproj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
