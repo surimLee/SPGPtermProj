187528d4fa55e4bd2003fcfcc005b5f3ab9c0171
surimLee <45868707+surimLee@users.noreply.github.com>
2020-06-21 23:34:25 +0900

    Add 3 Activity and Activity Transition

--- app/src/main/java/kr/ac/kpu/ce2016152030/spgptermproj/MainActivity.java
+++ app/src/main/java/kr/ac/kpu/ce2016152030/spgptermproj/MainActivity.java
@@ -2,7 +2,11 @@ package kr.ac.kpu.ce2016152030.spgptermproj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button withFriend = findViewById(R.id.bt1);
+        withFriend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TwoPlayerActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button withAi = findViewById(R.id.bt2);
+        withAi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, OnePlayerActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton imageButton = findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SettingActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/kr/ac/kpu/ce2016152030/spgptermproj/TwoPlayerActivity.java
@@ -0,0 +1,15 @@
+package kr.ac.kpu.ce2016152030.spgptermproj;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TwoPlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two_player);
+    }
+}
